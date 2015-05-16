23bf3f04888c5036ca801794dd60971a369b2bbc
ManSung-Kim <jactlab@gmail.com>
2015-05-16 23:33:35 +0900

    Create Dlog.java

---
+++ dbg_log/Dlog.java
@@ -0,0 +1,19 @@
+package dbg_log;
+
+import android.util.Log;
+
+public final class Dlog {
+	private static String tag = "mstag";
+	
+	public void setTag(String tag) {
+		Dlog.tag = tag;
+	}
+	
+	public static int d(String msg) { // only String class
+		return Log.d(tag, msg);
+	}
+	
+	public static <T extends Object> int d(T msg) { // Generic
+		return Log.d(tag, msg+"");
+	}
+}
